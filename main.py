a256c34fa0508340ff3e9b310cc31a88784565e9
Ondrej DRAGANOV <ondrej.draganov@ist.ac.at>
2024-01-23 11:40:58 +0100

    build: version 1.0.5 (plus minor main.py edits)

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from tests.test_bars import TestBars
-from chromatic_tda import ChromaticAlphaComplex, SimplicialComplex
+from chromatic_tda import ChromaticAlphaComplex, SimplicialComplex, plot_six_pack, plot_persistence_diagram
 import numpy as np
+from matplotlib import pyplot as plt
 
 
-def main():
+def run_test():
     TestBars().test_all(verbose=True, assertions=False)
     # for embedding in TestBars().single_test('two_circles_cc', return_detailed=True):
     #     for group, result in embedding.items():
@@ -26,6 +27,19 @@ def main():
     #     color_range_splits=(.3, .6),
     #     sub_complex='monochromatic'
     # ).run()
+
+
+def run_test_plot():
+    points = np.random.random((50, 2))
+    labels = list(map(int, 2 * np.random.random(len(points))))
+    cplx = ChromaticAlphaComplex(points, labels).get_simplicial_complex(sub_complex='0')
+    fig, axs = plt.subplots(3, 2, figsize=(10, 15))
+    plot_six_pack(cplx, axs = axs)
+    plt.show()
+
+
+def main():
+    run_test_plot()
 
 
 if __name__ == "__main__":
